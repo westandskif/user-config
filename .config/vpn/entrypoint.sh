7d7327966a479f446d03fec2e880c0ab559131ea
Nikita Almakov <nikita.almakov@gmail.com>
2025-07-30 16:48:46 +0300

    added own vpn handling as socks5 proxy for Firefox

--- .config/vpn/entrypoint.sh
+++ .config/vpn/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "100 openvpn" >> /etc/iproute2/rt_tables
 
 openvpn --config my_expressvpn_usa_-_washington_dc_udp.ovpn --auth-user-pass up.txt &
-until ip link show vpnbridge0 2>/dev/null; do sleep 1; done
+until ip link show tun0 2>/dev/null; do sleep 1; done
 danted &
 
 echo "SOCKS5 OVER VPN IS READY!"
--- .config/vpn/route-up.sh
+++ .config/vpn/route-up.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-source "`dirname $0`/vpn-config.sh" 
+source "`dirname $0`/vpn-config.sh"
 
 logger "OpenVPN route-up.sh: bridge if: $VPNBRIDGE_IF addr: $VPNBRIDGE_ADDR table: $VPNBRIDGE_TBL"
 
-# Add the default route to our custom 'openvpn' routing table
 ip route add default via $route_vpn_gateway dev $dev table $VPNBRIDGE_TBL
+ip rule add from $ifconfig_local lookup $VPNBRIDGE_TBL
 
-# Add new interface to act as a middleman between the fixed address used 
-# by Squid and the dynamic address provided by OpenVPN
-ip tuntap add dev $VPNBRIDGE_IF mode tun
-ip addr add $VPNBRIDGE_ADDR dev $VPNBRIDGE_IF
-ip link set dev $VPNBRIDGE_IF up
 
-# All packets from Squid are passed to the custom routing table
-ip rule add from $VPNBRIDGE_ADDR lookup $VPNBRIDGE_TBL
 
-# Rewrite the source address to match the one provided by OpenVPN
-iptables -t nat -A POSTROUTING -s $VPNBRIDGE_ADDR -j SNAT --to-source $ifconfig_local
 
-logger "OpenVPN route-up.sh: local: $ifconfig_local remote: $route_vpn_gateway device: $dev"
+# # Add the default route to our custom 'openvpn' routing table
+# ip route add default via $route_vpn_gateway dev $dev table $VPNBRIDGE_TBL
+#
+# # Add new interface to act as a middleman between the fixed address used
+# # by Squid and the dynamic address provided by OpenVPN
+# ip tuntap add dev $VPNBRIDGE_IF mode tun
+# ip addr add $VPNBRIDGE_ADDR dev $VPNBRIDGE_IF
+# ip link set dev $VPNBRIDGE_IF up
+#
+# # All packets from Squid are passed to the custom routing table
+# ip rule add from $VPNBRIDGE_ADDR lookup $VPNBRIDGE_TBL
+#
+# # Rewrite the source address to match the one provided by OpenVPN
+# iptables -t nat -A POSTROUTING -s $VPNBRIDGE_ADDR -j SNAT --to-source $ifconfig_local
+#
+# logger "OpenVPN route-up.sh: local: $ifconfig_local remote: $route_vpn_gateway device: $dev"
