7c7d391575bef12a785f2e658d70e47b98db5a6a
Nikita Almakov <nikita.almakov@gmail.com>
2025-09-15 02:56:48 +0300

    added http/https proxy too

--- .config/vpn/entrypoint.sh
+++ .config/vpn/entrypoint.sh
@@ -5,6 +5,7 @@ echo "100 openvpn" >> /etc/iproute2/rt_tables
 openvpn --config config.ovpn --auth-user-pass up.txt &
 until ip link show tun0 2>/dev/null; do sleep 1; done
 danted &
+privoxy --no-daemon /etc/privoxy/config &
 
 echo "SOCKS5 OVER VPN IS READY!"
 while true; do sleep 1000; done
