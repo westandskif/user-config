2f5616aed7a3e48359f6b8bc2a0e93d12c025013
Nikita Almakov <nikita.almakov@gmail.com>
2025-09-15 01:54:19 +0300

    vpn handling as proxy

--- .config/vpn/entrypoint.sh
+++ .config/vpn/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "100 openvpn" >> /etc/iproute2/rt_tables
 
-openvpn --config my_expressvpn_usa_-_washington_dc_udp.ovpn --auth-user-pass up.txt &
+openvpn --config config.ovpn --auth-user-pass up.txt &
 until ip link show tun0 2>/dev/null; do sleep 1; done
 danted &
 
