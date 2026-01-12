29dba616f060d36249d5136b3f97c39334b74e36
Nikita Almakov <nikita.almakov@gmail.com>
2026-01-12 22:26:45 +0300

    dropped vpn helpers

--- .config/vpn/route-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "`dirname $0`/vpn-config.sh"
-
-logger "OpenVPN route-up.sh: bridge if: $VPNBRIDGE_IF addr: $VPNBRIDGE_ADDR table: $VPNBRIDGE_TBL"
-
-ip route add default via $route_vpn_gateway dev $dev table $VPNBRIDGE_TBL
-ip rule add from $ifconfig_local lookup $VPNBRIDGE_TBL
--- .config/vpn/vpn-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export VPNBRIDGE_IF=vpnbridge0
-export VPNBRIDGE_ADDR=10.202.101.1
-export VPNBRIDGE_TBL=openvpn
